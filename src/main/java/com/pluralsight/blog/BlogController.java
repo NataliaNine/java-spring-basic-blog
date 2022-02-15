80fdac3d6304d4550a3e5d0227e7b161c222fb0c
NataliaNine <99683284+NataliaNine@users.noreply.github.com>
2022-02-15 11:29:59 -0500

    Update BlogController.java

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
     private PostRepository postRepository;
